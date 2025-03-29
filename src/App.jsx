a4116dd83cdfe1185b782bd4cb2f300c1f61707e
Manisha varma <manishavkapex1@gmail.com>
2025-03-29 18:33:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline bg-red-100">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
