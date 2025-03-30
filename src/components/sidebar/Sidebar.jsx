d5187aa70021c4c58472845a92f617ee89c5e650
Manisha varma <manishavkapex1@gmail.com>
2025-03-30 17:07:36 +0530

    plan component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AccountToggle from "./AccountToggle";
 import SearchBar from "./SearchBar";
 import RouteSelect from "./RouteSelect";
+import Plain from "./Plain";
 
 const Sidebar = () => {
   return (
@@ -14,6 +15,7 @@ const Sidebar = () => {
       </div>
 
       {/* toggle */}
+      <Plain />
     </div>
   );
 };
