85955a282ec5c89e63cfdbf82c80a1892728a26f
Manisha varma <manishavkapex1@gmail.com>
2025-04-05 14:08:40 +0530

    remove height

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     
     
-    <div className='bg-gray-900 rounded-lg shadow pb-4 h-[200vh]'>
+    <div className='bg-gray-900 rounded-lg shadow pb-4'>
         <TopBar />
         <Grid />
     </div>
